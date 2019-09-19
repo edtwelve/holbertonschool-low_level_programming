6b9be2ada7d33e87593cfab47aec379c5e0ab4c3
edtwelve <eamc201168@gmail.com>
2019-09-19 22:03:20 +0000

    fixed 0 to 99

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,26 +1,35 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Outputs numbers from 00 to 99 with comma and space in between
  *
- * Return: Always 0 (Success)
+ * Return: (0) Successful execution
  */
 
 int main(void)
 {
+	int a;
+	int b;
 
-int a;
+	for (a = 0; a <= 9; a++)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			b++;
 
-for (a = 0; a <= 99; a++)
-        putchar(a + '00');
-        if (a <= 99)
-                {
-                putchar(", ");
-                putchar(" ");
-                }
 
-        putchar('\n');
-        return (0);
+			if (a + b <= 18)
+			{
+				putchar(',');
+				putchar(' ');
+
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
 }
-
