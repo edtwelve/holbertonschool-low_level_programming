7f56e167a9af12ce837b96af73f12efd9a097a5a
edtwelve <eamc201168@gmail.com>
2019-09-19 20:26:17 +0000

    file fixed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char z;
+
+	for (z = 'z'; z >= 'a'; z--)
+	{
+	putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
+
