8ad8c07077e71be49e4726d31c621fb586ab0303
edtwelve <eamc201168@gmail.com>
2019-09-24 22:25:42 +0000

    indentation fix

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * add - this function adds two integers
+ *
+ *@o: first number to take
+ *@t: second nuber to take
+ *Return: the sum of two integers
+ *
+ */
+int add(int o, int t)
+{
+	int result = a + b;
+
+	return (result);
+}
+
