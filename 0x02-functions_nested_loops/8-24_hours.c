919c710ada773ed69534ee86e5d7a2b3ab06f442
edtwelve <eamc201168@gmail.com>
2019-09-24 15:33:09 +0000

    comments and structure fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,8 @@
 #include "holberton.h"
 /**
+ * jack_bauer: print every minute of the day of Jack Bauer.
  *
- *jack_bauer: print every minute of the day of Jack Bauer.
  *
- *
- * Return: last digit of a number
- * @i: number to evaluate 
  */
 
 void jack_bauer(void)
@@ -16,10 +13,10 @@ for (hr = 0; hr < 24; hr++)
 {
 	for (min = 0; min < 60; min++)
 	{
-	_putchar('0' + (hr/10));
+	_putchar('0' + (hr / 10));
 	_putchar('0' + (hr % 10));
 	_putchar(':');
-	_putchar('0' + (min/10));
+	_putchar('0' + (min / 10));
 	_putchar('0' + (min % 10));
 	_putchar('\n');
 	}
