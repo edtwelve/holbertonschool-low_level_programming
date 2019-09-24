67946ffa5ea85e5b483c94e2b01fb400428780b2
edtwelve <eamc201168@gmail.com>
2019-09-24 18:05:30 +0000

    structure and creation

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+void positive_or_negative(int i)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+}
