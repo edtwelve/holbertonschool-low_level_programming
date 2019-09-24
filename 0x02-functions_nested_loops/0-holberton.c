6ddbdecc38e7b4f1737b473a860bff996c662205
edtwelve <eamc201168@gmail.com>
2019-09-24 13:53:07 +0000

    file function fix

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,17 +2,13 @@
 
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
-_putchar('\n');
+char *h = "Holberton\n";
 
+while (*h)
+{
+	_putchar(*h);
+	h++;
+}
 return (0);
 }
 
