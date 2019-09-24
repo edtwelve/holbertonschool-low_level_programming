a643f334430e108cf0bb641fa3ed6011c10111f0
edtwelve <eamc201168@gmail.com>
2019-09-24 14:54:58 +0000

    file creation and fix

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *abs: compute absolute value of integer
+ *
+ *
+ * Return: absolute value of i
+ * @i: number to evaluate
+ */
+
+int _abs(int n)
+{
+
+if (n > 0)
+	{
+	return (n);
+	}
+else
+	{
+	return (-1 * n);
+	}
+}
+
