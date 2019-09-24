e6b2c22cd6c2928a9c22df14ef58f9c2beac4698
edtwelve <eamc201168@gmail.com>
2019-09-24 22:24:25 +0000

    creation and indentation config

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_to_98 - This function will print numbers..
+ * @n: reference variable
+ */
+
+void print_to_98(int n)
+{
+int o;
+
+if (n < 98)
+{
+	for (o = n; o <= 98; o++)
+	{
+		printf("%d", o);
+		if (o != 98)
+		{
+		printf(", ");
+		}
+	}
+printf("\n");
+}
+if (n > 98)
+{
+	for (o = n; o >= 98; o--)
+	{
+	printf("%d", o);
+		if (o != 98)
+		{
+		printf(", ");
+		}
+	}
+printf("\n");
+}
+}
