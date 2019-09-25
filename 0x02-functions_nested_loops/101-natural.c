0d0250487d68b5f1a7b35e08efb6192a96683245
edtwelve <eamc201168@gmail.com>
2019-09-25 00:27:35 +0000

    indentation fixed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	int a; 
-	int b = 0;
+int a;
+int b = 0;
 
-	    for(a = 0; a < 1024; a++)
-		        {
-				    if ((a % 3 == 0) || (a % 5 == 0))
-					        b = b + a;
-				    	}
-	       printf("%d\n", b);
-	          return(0);
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 3 == 0) || (a % 5 == 0))
+		b = b + a;
+	}
+	printf("%d\n", b);
+	return (0);
 }
 
