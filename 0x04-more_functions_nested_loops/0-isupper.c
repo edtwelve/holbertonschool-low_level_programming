bf788379d1ad53ba4cbc760451ec4ce11b69b2bd
edtwelve <eamc201168@gmail.com>
2019-09-26 15:06:38 +0000

    first file creation

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _isupper - check for uppercase character.
+ *
+ * Return: 1 if uppercase, 0 if otherwise.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else if (c >= 'a' && c <= 'z')
+		return (0);
+
+return (0);
+}
+
