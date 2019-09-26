98b7ce458fccdb2a01ed63b4fd66724d17600dda
edtwelve <eamc201168@gmail.com>
2019-09-26 15:45:09 +0000

    creation and structure fix

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  *print_numbers - print numbers from 0 to 9..
+  *
+  * .
+  */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
