6cb652d1c70d23db19bbe7e7424052979b8f3031
edtwelve <eamc201168@gmail.com>
2019-10-01 03:54:27 +0000

    file creation

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+  * print_env - function that prints a string in reverse..
+  *
+  *@s: string that will be printed 
+  */
+
+void print_rev(char *s)
+{
+	int a;
+
+	a = _strlen(s);
+	
+	while (a != 0)
+	{
+		_putchar(s[a]);
+		a--;
+	}
+	_putchar(s[0]);
+	_putchar('\n');
+}
+
