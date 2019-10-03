d3b6aae39bf763f4ca505f3c95f3ca00dc3d3ae1
edtwelve <eamc201168@gmail.com>
2019-10-03 21:28:15 +0000

    structure fix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,19 +3,20 @@
 /**
  * string_toupper - function that changes all lowercases letters to uppercases
  * @onetwo : array of chharacteers
+ * Return: array content
  */
 
 char *string_toupper(char *onetwo)
 {
 	int c = 0;
-	
-	for(c = 0; onetwo[c] != '\0'; c++)
+
+	for (c = 0; onetwo[c] != '\0'; c++)
 	{
-		if(onetwo[c] > 96 && onetwo[c] < 123) 
-		{	
+		if (onetwo[c] > 96 && onetwo[c] < 123)
+		{
 			onetwo[c] -= 32;
 		}
-	}	
+	}
 	onetwo[c] = '\0';
 	return (onetwo);
 }
