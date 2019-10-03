ec3fb254af4d86afa9019e7e973e29739d4a4377
edtwelve <eamc201168@gmail.com>
2019-10-03 19:29:09 +0000

    file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares between strings
+ *
+ * @s1: first array of integers
+ * @s2: second array of integers
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int cmp = (*s1-*s2);
+	while( ( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	if(*s1 == *s2) 
+	{
+		return (0);
+	}
+	else
+		return(cmp);
+
+}
+
