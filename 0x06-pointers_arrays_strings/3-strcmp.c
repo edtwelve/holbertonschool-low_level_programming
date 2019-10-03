df8fbdb9506dbebd1317579a365662b63b5c5dd4
edtwelve <eamc201168@gmail.com>
2019-10-03 19:33:31 +0000

    structure fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,23 +5,25 @@
  *
  * @s1: first array of integers
  * @s2: second array of integers
+ * Return: result of comparison
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int cmp = (*s1-*s2);
-	while( ( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2)
+	int cmp = (*s1 - *s2);
+
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
 
-	if(*s1 == *s2) 
+	if (*s1 == *s2)
 	{
 		return (0);
 	}
 	else
-		return(cmp);
+		return (cmp);
 
 }
 
