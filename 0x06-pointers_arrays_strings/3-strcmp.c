5c1fdfc754f92170459ae6b3fc9b3180ade94825
edtwelve <eamc201168@gmail.com>
2019-10-03 20:17:09 +0000

    file fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int c; cmp = (s1[c] - s2[c]);
-	
+	int c, cmp;
 
 	for (c = 0; s1[c] != '\0' && s2[c] != '\0'; c++)
 	{
+		cmp = (s1[c] - s2[c]);
 		if (cmp != 0)
 		{
 		return (cmp);
