57346e20c268069ed0b92fb6aad1550d8855aa10
edtwelve <eamc201168@gmail.com>
2019-10-03 19:48:14 +0000

    fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,15 +12,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int cmp = (*s1 - *s2);
 
-	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	while (*s1 != '\0' && *s2 != '\0')
 	{
 		s1++;
 		s2++;
-	}
-
 	if (*s1 != *s2)
 	{
 		return (cmp);
+	}
 	}
 	return (0);
 }
