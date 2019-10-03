037efe1347551e4b068876c103c8cd2ba1dfbb0d
edtwelve <eamc201168@gmail.com>
2019-10-03 21:25:28 +0000

    file creation

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - function that changes all lowercases letters to uppercases
+ * @onetwo : array of chharacteers
+ */
+
+char *string_toupper(char *onetwo)
+{
+	int c = 0;
+	
+	for(c = 0; onetwo[c] != '\0'; c++)
+	{
+		if(onetwo[c] > 96 && onetwo[c] < 123) 
+		{	
+			onetwo[c] -= 32;
+		}
+	}	
+	onetwo[c] = '\0';
+	return (onetwo);
+}
+
